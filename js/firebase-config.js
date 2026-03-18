b1b89f5c3878fbf37ce5586cc6bcedddf46fecf9
B-kaluwa <102986097+B-kaluwa@users.noreply.github.com>
2026-03-18 09:55:02 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,24 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Enable offline persistence
+db.enablePersistence()
+    .catch((err) => {
+        if (err.code == 'failed-precondition') {
+            console.log('Multiple tabs open, persistence enabled in first tab only');
+        } else if (err.code == 'unimplemented') {
+            console.log('Browser doesn\'t support persistence');
+        }
+    });
